2cefb6cb4db3a824a83b3f06a00da8240ad6d6ff
Brijrajsinh parmar <73275164+brijrajparmar27@users.noreply.github.com>
2024-04-24 12:33:03 +0530

    decomposition files continue

---
+++ src/Pages/Home/Components/FolderList.jsx
@@ -0,0 +1,72 @@
+import { AnimatePresence, motion } from "framer-motion";
+import { RiCloseCircleFill } from "react-icons/ri";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteFolder, setActiveFolder } from "../../../redux/features/folderSlice";
+import { fetchTodos } from "../../../redux/features/todoSlice";
+import { STATUS_ALL } from "../../../utils/constants";
+
+export const FolderList = ({ folderText }) => {
+  const dispatch = useDispatch();
+  const { folderList, selectedFolder } = useSelector(
+    (state) => state.folderReducer
+  );
+
+  const handleDelete = async (folderId) => {
+    const searchText = folderText;
+    console.log(searchText);
+    dispatch(deleteFolder({ folderId, searchText }));
+  };
+
+  const handleSelect = async (folder) => {
+    if (selectedFolder._id !== folder._id) {
+      await dispatch(setActiveFolder(folder));
+      dispatch(fetchTodos({ status: STATUS_ALL }));
+    }
+  };
+  return (
+    <>
+      {folderList?.map((each) => {
+        return (
+          <AnimatePresence mode="popLayout">
+            <motion.div
+              className="flex items-center w-full justify-between px-3 h-12 cursor-pointer"
+              key={each._id}
+              onClick={() => {
+                handleSelect(each);
+              }}
+            >
+              <p>{each?.name}</p>
+              <div className="flex gap-1">
+                <AnimatePresence mode="popLayout">
+                  {selectedFolder?._id === each._id && (
+                    <motion.div
+                      className="bg-green px-3 rounded-3xl"
+                      initial={{ x: -100, opacity: 0 }}
+                      animate={{ x: 0, opacity: 1 }}
+                      exit={{ x: -100, opacity: 0 }}
+                    >
+                      <p className="text-white text-content">Selected</p>
+                    </motion.div>
+                  )}
+                </AnimatePresence>
+                <AnimatePresence mode="popLayout">
+                  {!(each.isDefault || selectedFolder?._id === each._id) && (
+                    <motion.div>
+                      <RiCloseCircleFill
+                        className="text-title text-gray cursor-pointer"
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          handleDelete(each._id);
+                        }}
+                      />
+                    </motion.div>
+                  )}
+                </AnimatePresence>
+              </div>
+            </motion.div>
+          </AnimatePresence>
+        );
+      })}
+    </>
+  );
+};
--- src/Pages/Home/Components/FolderSwitcher.jsx
+++ src/Pages/Home/Components/FolderSwitcher.jsx
@@ -1,24 +1,13 @@
-import { RiCloseCircleFill } from "react-icons/ri";
 import { AnimatePresence, motion } from "framer-motion";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  createFolder,
-  deleteFolder,
-  findFolder,
-  setActiveFolder,
-} from "../../../redux/features/folderSlice";
-import { fetchTodos } from "../../../redux/features/todoSlice";
-import { STATUS_ALL } from "../../../utils/constants";
+import { useDispatch } from "react-redux";
+import { createFolder, findFolder } from "../../../redux/features/folderSlice";
 import { useEffect, useState } from "react";
+import { FolderList } from "./FolderList";
 
 export const FolderSwitcher = ({ toogleShow }) => {
   const [folderText, setFolderText] = useState("");
   const dispatch = useDispatch();
   const [createPromptVisible, setCreatePromptVisible] = useState(false);
-
-  const { folderList, selectedFolder } = useSelector(
-    (state) => state.folderReducer
-  );
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -70,22 +59,9 @@ export const FolderSwitcher = ({ toogleShow }) => {
     setFolderText(testInput);
   };
 
-  const handleSelect = async (folder) => {
-    if (selectedFolder._id !== folder._id) {
-      await dispatch(setActiveFolder(folder));
-      dispatch(fetchTodos({ status: STATUS_ALL }));
-    }
-  };
-
   const handleAddFolder = async () => {
     await dispatch(createFolder(folderText));
     setCreatePromptVisible(false);
-  };
-
-  const handleDelete = async (folderId) => {
-    const searchText = folderText;
-    console.log(searchText);
-    dispatch(deleteFolder({ folderId, searchText }));
   };
 
   const createPromptVariant = {
@@ -159,50 +135,8 @@ export const FolderSwitcher = ({ toogleShow }) => {
                 </motion.div>
               )}
             </AnimatePresence>
-            {folderList?.map((each) => {
-              return (
-                <AnimatePresence mode="popLayout">
-                  <motion.div
-                    className="flex items-center w-full justify-between px-3 h-12 cursor-pointer"
-                    key={each._id}
-                    onClick={() => {
-                      handleSelect(each);
-                    }}
-                  >
-                    <p>{each?.name}</p>
-                    <div className="flex gap-1">
-                      <AnimatePresence mode="popLayout">
-                        {selectedFolder?._id === each._id && (
-                          <motion.div
-                            className="bg-green px-3 rounded-3xl"
-                            initial={{ x: -100, opacity: 0 }}
-                            animate={{ x: 0, opacity: 1 }}
-                            exit={{ x: -100, opacity: 0 }}
-                          >
-                            <p className="text-white text-content">Selected</p>
-                          </motion.div>
-                        )}
-                      </AnimatePresence>
-                      <AnimatePresence mode="popLayout">
-                        {!(
-                          each.isDefault || selectedFolder?._id === each._id
-                        ) && (
-                          <motion.div>
-                            <RiCloseCircleFill
-                              className="text-title text-gray cursor-pointer"
-                              onClick={(e) => {
-                                e.stopPropagation();
-                                handleDelete(each._id);
-                              }}
-                            />
-                          </motion.div>
-                        )}
-                      </AnimatePresence>
-                    </div>
-                  </motion.div>
-                </AnimatePresence>
-              );
-            })}
+
+            <FolderList folderText={folderText} />
           </motion.div>
         </div>
       </motion.div>
